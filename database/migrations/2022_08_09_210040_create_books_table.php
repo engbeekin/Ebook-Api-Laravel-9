ac51cd7f5404a187ecf4b6f8c00a277af5b3d3c1
kalid ahmed <khalidbeekin1@gmail.com>
2022-08-09 22:08:03 +0100

    added model & controllers with it's tables and migrated to Db

---
+++ database/migrations/2022_08_09_210040_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('file_upload')->nullable();
+            $table->integer('pages')->nullable();
+            $table->string('publishing_year')->nullable();
+            $table->foreignId('language_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
